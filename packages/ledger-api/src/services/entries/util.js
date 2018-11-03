206b254fc7c354a5e070bb8dc712c10aa7c99657
mkim5 <matthewkim93@gmail.com>
2018-11-03 18:15:35 -0500

    add raw transaction string to entries util

--- packages/ledger-api/src/services/entries/util.js
+++ packages/ledger-api/src/services/entries/util.js
@@ -5,7 +5,23 @@ const hl = require('hledger');
 
 const loadEntries = async (file) => hl.tableize(await hl(['-f', file, 'print']));
 
-const normalizeTransactions = async (txns) => R.groupBy((txn) => txn.txnidx)(await txns);
+const normalizeTransactions = async (txns) => {
+  const groupByTransactions = R.groupBy((txn) => txn.txnidx);
+  const generateRawTransaction = R.reduce((a, b) => {
+    const entryAsString = `  ${b.account}  ${b.commodity}${b.amount}`;
+    return R.isNil(a) ?
+      `${b.date} ${b.description}\n  ${entryAsString}` :
+      `${a}\n  ${entryAsString}`;
+  }, null);
+  const addRawTransactionToTransactionMap = R.map((txn) => (
+    {
+      transactions: txn,
+      rawTransaction: generateRawTransaction(txn)
+    }
+  ));
+
+  return R.compose(addRawTransactionToTransactionMap, groupByTransactions)(await txns);
+}
 
 module.exports = {
   loadEntries,
